3a9b538372a1342c6cfe0bde3ebc64ba2b8a8bad
walterajax <waltervk1995@hotmail.com>
2020-10-20 17:25:27 +0200

    add detail page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-var url = 'https://walterajax.github.io/frontenddevelopment/'
+var url = 'http://127.0.0.1:58081/'
 
 var hamburgerIcon = document.querySelector('#hamburger_icon');
 var hamburgerMenu= document.querySelector('#hamburger_menu');
